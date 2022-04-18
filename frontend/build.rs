0c19a4f3401d898fb8b6e31be8a3ad0662e1b3ca
David Gregory <davidgregory084@gmail.com>
2022-04-18 11:04:48 +0100

    Start to add lexer and parser

--- frontend/build.rs
+++ frontend/build.rs
@@ -1,6 +1,16 @@
-use std::io::Result;
+use cfgrammar::yacc::YaccKind;
+use lrlex::CTLexerBuilder;
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    CTLexerBuilder::new()
+        // .lrpar_config(|ctp| {
+        //     ctp.yacckind(YaccKind::Grmtools)
+        //         .grammar_in_src_dir("mina.y")
+        //         .unwrap()
+        // })
+        .lexer_in_src_dir("mina.l")?
+        .build()?;
+
     prost_build::compile_protos(
         &["src/ast.proto"],
         &["src"]
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufRead, Result, Write};
+
+use lrlex::lrlex_mod;
+use lrpar::Lexer;
+// use lrpar::lrpar_mod;
+
+lrlex_mod!("mina.l");
+// lrpar_mod!("mina.y");
+
+fn main() -> Result<()> {
+    let lexerdef = mina_l::lexerdef();
+    let stdin = std::io::stdin();
+
+    loop {
+        io::stdout().flush()?;
+
+        match stdin.lock().lines().next() {
+            Some(Ok(ref l)) => {
+                if l.trim().is_empty() {
+                    continue;
+                }
+
+                let lexer = lexerdef.lexer(l);
+
+                lexer.iter().for_each(|result| match result {
+                    Ok(lexeme) => println!("{:?}", lexeme),
+                    Err(err) => println!("{:?}", err),
+                });
+            }
+            _ => break,
+        }
+    }
+
+    Ok(())
 }
