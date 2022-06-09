637592f824c824ad90148af6bccefaa0441a97fa
DavidGregory084 <davidgregory084@gmail.com>
2022-06-09 22:51:04 +0000

    Add tests for VS Code extension

---
+++ vscode-plugin/src/test/runTest.ts
@@ -0,0 +1,48 @@
+import * as cp from "child_process";
+import * as path from "path";
+
+import {
+  downloadAndUnzipVSCode,
+  runTests,
+  resolveCliArgsFromVSCodeExecutablePath,
+} from "@vscode/test-electron";
+import { TestOptions } from "@vscode/test-electron/out/runTest";
+
+async function main() {
+  try {
+    // The folder containing the Extension Manifest package.json
+    // Passed to `--extensionDevelopmentPath`
+    const extensionDevelopmentPath = path.resolve(__dirname, "../../");
+
+    // The path to the extension test script
+    // Passed to --extensionTestsPath
+    const extensionTestsPath = path.resolve(__dirname, "./suite/index");
+
+    const testOptions: TestOptions = {
+      extensionDevelopmentPath,
+      extensionTestsPath,
+    };
+
+    const vscodeExecutablePath = await downloadAndUnzipVSCode(testOptions);
+
+    const [cli, ...args] =
+      resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
+
+    cp.spawnSync(
+      cli,
+      [...args, "--install-extension", "mindaro-dev.file-downloader"],
+      {
+        encoding: "utf-8",
+        stdio: "inherit",
+      }
+    );
+
+    // Download VS Code, unzip it and run the integration test
+    await runTests(testOptions);
+  } catch (err) {
+    console.error("Failed to run tests");
+    process.exit(1);
+  }
+}
+
+main();
---
+++ vscode-plugin/src/test/suite/extension.test.ts
@@ -0,0 +1,16 @@
+import * as assert from "assert";
+import * as vscode from "vscode";
+
+suite("Mina Language Extension", () => {
+  test("should be present", () => {
+    assert.ok(vscode.extensions.getExtension("mina-lang.mina-lang-vscode"));
+  });
+
+  test("should activate successfully", async () => {
+    await vscode.extensions
+      .getExtension("mina-lang.mina-lang-vscode")
+      .activate();
+
+    return assert.ok(true);
+  }).timeout(30000);
+});
