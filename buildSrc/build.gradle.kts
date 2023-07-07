b29b8a1c0ce7321e2d65c76fc6781e0f8277f558
David Gregory <dgregory@noreply.codeberg.org>
2023-07-07 10:50:56 +0100

    Setup buildSrc Gradle Kotlin formatting

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,10 +1,24 @@
 plugins {
     `kotlin-dsl`
+    alias(libs.plugins.spotless)
 }
 
 repositories {
     mavenCentral()
     gradlePluginPortal()
+}
+
+spotless {
+    ratchetFrom("origin/main")
+
+    kotlinGradle {
+        encoding("UTF-8")
+        target("**/*.gradle.kts")
+        ktfmt().kotlinlangStyle()
+        indentWithSpaces()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
 }
 
 dependencies {
