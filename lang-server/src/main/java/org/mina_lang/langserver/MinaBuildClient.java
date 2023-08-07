65bbaba262e378be103af21834f2b545f3ad63c9
David Gregory <dgregory@noreply.codeberg.org>
2023-08-07 11:03:17 +0100

    Start to add some progress reporting

--- lang-server/src/main/java/org/mina_lang/langserver/MinaBuildClient.java
+++ lang-server/src/main/java/org/mina_lang/langserver/MinaBuildClient.java
@@ -5,9 +5,14 @@
 package org.mina_lang.langserver;
 
 import ch.epfl.scala.bsp4j.*;
+import ch.epfl.scala.bsp4j.PublishDiagnosticsParams;
+import org.eclipse.lsp4j.*;
+import org.eclipse.lsp4j.jsonrpc.messages.Either;
 import org.eclipse.lsp4j.services.LanguageClient;
 
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Future;
 
 public class MinaBuildClient implements BuildClient {
@@ -18,13 +23,11 @@ public class MinaBuildClient implements BuildClient {
     private Process buildServerProcess;
     private Future<Void> listenerFuture;
 
-    public MinaBuildClient(MinaLanguageServer languageServer) {
-        this.languageServer = languageServer;
-        this.languageClient = languageServer.getClient();
-    }
+    private ConcurrentHashMap<TaskId, CompletableFuture<Void>> progressReports = new ConcurrentHashMap<>();
 
     public MinaBuildClient(MinaLanguageServer languageServer, Process buildServerProcess) {
-        this(languageServer);
+        this.languageServer = languageServer;
+        this.languageClient = languageServer.getClient();
         this.buildServerProcess = buildServerProcess;
     }
 
@@ -45,21 +48,58 @@ public class MinaBuildClient implements BuildClient {
 
     @Override
     public void onBuildTaskStart(TaskStartParams params) {
-        // TODO Auto-generated method stub
-
-
+        languageServer.ifShouldNotify(() -> {
+            var progressToken = Either.<String, Integer>forLeft(params.getTaskId().getId());
+            var progressCreateParams = new WorkDoneProgressCreateParams(progressToken);
+            var progressFuture = languageClient.createProgress(progressCreateParams);
+            progressReports.put(params.getTaskId(), progressFuture);
+            progressFuture.thenRun(() -> {
+                var progressReport = new WorkDoneProgressBegin();
+                progressReport.setTitle(params.getMessage());
+                progressReport.setPercentage(0);
+                var progressReportEither = Either.forLeft((WorkDoneProgressNotification) progressReport);
+                var progressParams = new ProgressParams(progressToken, progressReportEither);
+                languageClient.notifyProgress(progressParams);
+            });
+        });
     }
 
     @Override
     public void onBuildTaskProgress(TaskProgressParams params) {
-        // TODO Auto-generated method stub
-
+        languageServer.ifShouldNotify(() -> {
+            Optional
+                .ofNullable(progressReports.get(params.getTaskId()))
+                .ifPresent(future -> {
+                    future.thenRun(() -> {
+                        var progressToken = Either.<String, Integer>forLeft(params.getTaskId().getId());
+                        var progressReport = new WorkDoneProgressReport();
+                        progressReport.setMessage(params.getMessage());
+                        progressReport.setPercentage(params.getProgress().intValue());
+                        var progressReportEither = Either.forLeft((WorkDoneProgressNotification) progressReport);
+                        var progressParams = new ProgressParams(progressToken, progressReportEither);
+                        languageClient.notifyProgress(progressParams);
+                    });
+                });
+        });
     }
 
     @Override
     public void onBuildTaskFinish(TaskFinishParams params) {
-        // TODO Auto-generated method stub
-
+        languageServer.ifShouldNotify(() -> {
+            Optional
+                .ofNullable(progressReports.get(params.getTaskId()))
+                .ifPresent(future -> {
+                    progressReports.remove(params.getTaskId());
+                    future.thenRun(() -> {
+                        var progressToken = Either.<String, Integer>forLeft(params.getTaskId().getId());
+                        var progressReport = new WorkDoneProgressEnd();
+                        progressReport.setMessage(params.getMessage());
+                        var progressReportEither = Either.forLeft((WorkDoneProgressNotification) progressReport);
+                        var progressParams = new ProgressParams(progressToken, progressReportEither);
+                        languageClient.notifyProgress(progressParams);
+                    });
+                });
+        });
     }
 
     @Override
@@ -76,10 +116,6 @@ public class MinaBuildClient implements BuildClient {
     @Override
     public void onConnectWithServer(BuildServer server) {
         this.buildServer = server;
-    }
-
-    public void onStartProcess(Process buildServerProcess) {
-        this.buildServerProcess = buildServerProcess;
     }
 
     public void onStartListening(Future<Void> listenerFuture) {
