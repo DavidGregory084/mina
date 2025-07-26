72061808f738903c0d236870f5127ef73148799b
David Gregory <dgregory@noreply.codeberg.org>
2025-07-26 15:43:57 +0100

    Consume gradle-bsp snapshots from the Central Portal

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 repositories {
     mavenLocal()
     mavenCentral()
-    maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
+    maven { url = uri("https://central.sonatype.com/repository/maven-snapshots/") }
 }
 
 val pluginId = "org.mina-lang.gradle"
